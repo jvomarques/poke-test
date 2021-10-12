70a604d20804f8c1ef7dc3e62392e086c01aca11
Joao Victor <joaovictor.marques@thoughtworks.com>
2021-10-12 17:05:55 -0300

    adding input type pokemon name

--- src/screens/home/presentation/__tests__/home.screen.test.tsx
+++ src/screens/home/presentation/__tests__/home.screen.test.tsx
@@ -1,15 +1,19 @@
 import { render, screen } from '@testing-library/react'
 
-import Home from './home.screen'
+import Home from '../home.screen'
 
 describe('<Home />', () => {
   describe('when home is renderes', () => {
-    beforeAll(() => {
+    beforeEach(() => {
       render(<Home />)
     })
-    it('renders the poke test image', () => {
+    it('renders the poke test logo image', () => {
       const pokeTestImage = screen.getByRole('img', { name: 'PokeTest Logo Image' })
       expect(pokeTestImage).toBeInTheDocument()
+    })
+    it('renders the search pokemon input', () => {
+      const searchPokemonInput = screen.getByPlaceholderText('Type a pokemon name')
+      expect(searchPokemonInput).toBeInTheDocument()
     })
   })
 })
--- src/screens/home/presentation/home.screen.tsx
+++ src/screens/home/presentation/home.screen.tsx
@@ -6,8 +6,11 @@ import style from './home.module.scss'
 function Home(): ReactElement {
   return (
     <div>
-      <div className={style.pokeTestLogoContainer}>
+      <div className={style.logoContainer}>
         <img src={pokeTestLogo} aria-label="PokeTest Logo Image" />
+      </div>
+      <div className={style.inputContainer}>
+        <input type="text" placeholder="Type a pokemon name" aria-label="Type a pokemon name" />
       </div>
     </div>
   )
