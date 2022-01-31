72346e6db24dc828f13e317be597312c68b53825
Joao Victor <joaovictor.marques@thoughtworks.com>
2022-01-31 15:24:55 -0300

    adding first test

---
+++ src/screens/home/domain/__tests__/podemon-defense-usecase.test.ts
@@ -0,0 +1,14 @@
+import calculateDefense from '../usecases/pokemon-defense-usecase'
+
+describe('PokemonDefenseUseCase', () => {
+  describe('when defense is calculated', () => {
+    it('returns 40% of attack value', () => {
+      const attack = 500
+      const defenseExpected = attack * 0.4
+
+      const defenseCalculated = calculateDefense(attack)
+
+      expect(defenseCalculated).toBe(defenseExpected)
+    })
+  })
+})
--- src/screens/home/domain/usecases/pokemon-defense-usecase.ts
+++ src/screens/home/domain/usecases/pokemon-defense-usecase.ts
@@ -1,4 +1,4 @@
 function calculateDefense(attack: number): number {
-  return attack
+  return attack * 0.4
 }
 export default calculateDefense
